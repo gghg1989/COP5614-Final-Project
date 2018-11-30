4cf3b1790b0115c4fb3205acede3dde24b62dd20
gghg1989 <gghg1989@gmail.com>
2018-11-30 17:07:51 -0500

    rm bin folder

---
+++ print/auto_run.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "os/exec"
+	"time"
+)
+
+func main(){
+
+for{
+    cmd := exec.Command("./lkm_print")
+
+        err := cmd.Run()
+        if err != nil {
+            fmt.Println("Execute Command failed:" + err.Error())
+            return
+        }
+        fmt.Println("lkm print loaded.")
+        time.Sleep(time.Duration(3)*time.Second)
+    }
+}
+
